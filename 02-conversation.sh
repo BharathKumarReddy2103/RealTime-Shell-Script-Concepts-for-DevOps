d787a9ecf13413c3f209fc68434483bcdd08443e
BharathKumarReddy <nbkumar2103@gmail.com>
2025-05-15 17:56:56 +0530

    updated 02-conversation.sh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Trump:: Hey Musk, How are you?"
-echo "Musk:: Hello Trump, I am fine. How are you doing?"
-echo "Trump:: I am fine too. Whats up?"
-echo "Musk:: Nothing, just going to mars now, will you come?"
-echo "Trump:: Sorry, you carry on! I will come once you come back"
+#!/bin/bash
+
+echo "Trump:: \"Hey Musk, How are you?\""
+echo "Musk:: \"Hello Trump, I am fine. How are you doing?\""
+echo "Trump:: \"I am fine too. What's up?\""
+echo "Musk:: \"Nothing, just going to Mars now. Will you come?\""
+echo "Trump:: \"Sorry, you carry on! I will come once you come back.\""
