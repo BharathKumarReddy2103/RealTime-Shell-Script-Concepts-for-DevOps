8b0525e42d4880b3a6a947c9510dd8506c3ab259
BharathKumarReddy <nbkumar2103@gmail.com>
2025-05-16 16:14:55 +0530

    updated 10-functions.sh

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,49 +2,45 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
+if [ "$USERID" -ne 0 ]; then
     echo "ERROR: Please run this script with root access"
     exit 1
 else
     echo "You are running with root access"
 fi
 
-VALIDATE()
-if [ $? -eq 0 ]
-then
-    echo "Installing $2 is ... SUCCESS"
-else
-    echo "Installing $2 is ... FAILURE
-    exit 1
-fi
-}
-
-dnf list installed mysql
-if [ $? -eq 0 ]
-then
-    echo "MYSQL is not installed... going to install it"
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
-else
-    echo "MYSQL is already installed...Nothing to do"
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo "Installing $2 is ... SUCCESS"
+    else
+        echo "Installing $2 is ... FAILURE"
+        exit 1
     fi
 }
 
-dnf list installed python3
-if [ $? -eq 0 ]
-then
-    echo "python3 is not installed... going to install it"
-    dnf install python3 -y
-    VALIDATE $? "python3"
+dnf list installed mysql > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
 else
-    echo "python3 is already installed...Nothing to do"
+    echo "MySQL is already installed... Nothing to do"
+fi
 
-dnf list installed nginx
-if [ $? -eq 0 ]
-then
-    echo "nginx is not installed... going to install it"
-    dnf install nginx -y
-    VALIDATE $? "nginx"
+dnf list installed python3 > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "Python3"
 else
-    echo "nginx is already installed...Nothing to do"
+    echo "Python3 is already installed... Nothing to do"
+fi
+
+dnf list installed nginx > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "Nginx is already installed... Nothing to do"
 fi
