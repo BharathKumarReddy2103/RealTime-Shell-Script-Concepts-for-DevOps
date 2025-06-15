e6042adedc606aab850a9b7facca4755745c687e
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-15 12:30:15 +0530

    updated .sh file

--- 07-array.sh
+++ 07-array.sh
@@ -3,6 +3,6 @@
 GAMES=("CRICKET" "FOOTBALL" "HOCKEY" "TENNIS")
 
 echo "First GAME: ${GAMES[0]}"
-echo "Last GAME: ${GAMES[3]}"
-echo "Last GAME: ${GAMES[4]}"
-echo "All GAMES: ${GAMES[@]}"
+# echo "Last GAME: ${GAMES[3]}"
+# echo "Last GAME: ${GAMES[4]}"
+# echo "All GAMES: ${GAMES[@]}"
