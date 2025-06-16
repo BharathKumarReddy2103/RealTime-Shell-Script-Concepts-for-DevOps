453fc0fb90cf97162ed750da87523e8cc8f162e1
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-16 05:55:19 +0530

    updated .sh file

--- 14-logs.sh
+++ 14-logs.sh
@@ -9,8 +9,8 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-# mkdir -p $LOGS_FOLDER
-# echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; 
 then
