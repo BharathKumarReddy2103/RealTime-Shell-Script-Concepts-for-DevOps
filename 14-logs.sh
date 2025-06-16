a13213e86b460756385d81115aafd085aec1d523
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-16 05:53:03 +0530

    updated .sh file

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,9 +5,9 @@ R=\e[31m"
 G=\e[32m"
 Y=\e[33m"
 N=\e[0m"
-# LOGS_FOLDER="/var/log/shellscript-logs"
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
