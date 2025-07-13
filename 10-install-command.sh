1230c40e0dfc80ba7df7aebd9d70c51c585ae840
BharathKumarReddy <nbkumar2103@gmail.com>
2025-07-13 20:04:33 +0530

    updated script

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -23,7 +23,7 @@ dnf list installed mysqldlkhl -y
         echo "Installing MYSQL is ... SUCCESS"
     else
         echo "Installing MYSQL is ... FAILURE"
-    # exit 1
+    exit 1
     fi
 # else
 #     echo "MYSQL is already installed...Nothing to do"
